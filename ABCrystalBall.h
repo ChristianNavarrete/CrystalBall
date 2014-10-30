4eac060c3c2f60262cc31b4e113fd2de5a9daf71
Christian Navarrete <idinkyy@gmail.com>
2014-10-29 20:40:24 -0700

    Done with Crystal Ball for now

---
+++ ABCrystalBall.h
@@ -0,0 +1,13 @@
+//
+//  ABCrystalBall.h
+//  CrystalBall
+//
+//  Created by HoodsDream on 9/4/14.
+//  Copyright (c) 2014 Asteroid Blues. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABCrystalBall : NSObject
+
+@end
---
+++ ABCrystalBall.m
@@ -0,0 +1,13 @@
+//
+//  ABCrystalBall.m
+//  CrystalBall
+//
+//  Created by HoodsDream on 9/4/14.
+//  Copyright (c) 2014 Asteroid Blues. All rights reserved.
+//
+
+#import "ABCrystalBall.h"
+
+@implementation ABCrystalBall
+
+@end
